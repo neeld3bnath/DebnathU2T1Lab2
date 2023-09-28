48f66793918d7538f67bb54763d9d3ff739ad888
Neel Debnath <neeld3@nycstudents.net>
2023-09-28 08:48:52 -0400

    Final

--- src/Circle.java
+++ src/Circle.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Circle {
+
+    // instance variables
+    private double radius;
+
+    // constructor
+    public Circle(double rad) {
+        radius = rad;
+    }
+
+    // method to print area
+    public void printArea() {
+        double area = 3.14 * radius * radius;
+        System.out.println("My area is: " +  area);
     }
 }
--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle = new Circle(5.0);
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+        //The area is 78.5
+        // D. run the code and confirm your prediction! Were you correct?
+    }
 }
